726d106b3b1c13ccad636d193d4fe7a8b2890227
Seho Tanaka <hellohelloseho@gmail.com>
2020-05-23 18:07:28 +0900

    fix

--- XOR physical representator/xor.py
+++ XOR physical representator/xor.py
@@ -13,7 +13,7 @@ def xor (argv):
         ans = bin(charList[i] ^ XOR_base)[2::] # XOR base is 9
         bin_ans = int(ans,2)
 
-        print("bit-wise operation XOR >>> %s(%d) ^ %s(9)" % (bin(charList[i])[2::],charList[i], bin(XOR_base)[2::]))
+        print("bit-wise operation XOR >>> %s(%d) ^ %s(%d)" % (bin(charList[i])[2::],charList[i], bin(XOR_base)[2::],XOR_base))
         print("result = %s(%d) = %s" % (ans,bin_ans, chr(bin_ans)))
         string_box.append(chr(bin_ans))
         print("string: %s" % ''.join(string_box))
